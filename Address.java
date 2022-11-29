8cbb4f8c4230d8468cb403df4dfaa524740549fa
rahulmaurya11 <rahulmaurya1500@gmail.com>
2022-11-29 11:45:30 +0530

    inheritance prac with kunal and self

---
+++ Address.java
@@ -0,0 +1,13 @@
+package BankAccount;
+
+public class Address {
+    String f,city,state,country;
+
+    Address(String f,String city,String state,String country){
+     this.f=f;
+     this.city=city;
+     this.state=state;
+     this.country=country;
+    }
+
+}
